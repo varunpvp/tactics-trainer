849c76a6204dddd41186959f49ebe60b6624c4cf
Varun Pujari <varunpvp@gmail.com>
2020-12-21 15:48:14 +0530

    create tactic board and test out simple tactics

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TacticBoard from "./components/TacticBoard";
+import Tactic from "./types/Tactic";
+import "./App.css";
+import { useState } from "react";
+
+const tactics: Tactic[] = [
+  {
+    id: "1",
+    fen: "rnbqkb1r/ppp2ppp/5n2/3Pp3/4P3/2N5/PP3PPP/R1BQKBNR b KQkq - 0 6",
+    blunderMove: "Bb4",
+    solution: "Qa4+",
+  },
+  {
+    id: "2",
+    fen: "6k1/1N6/p7/8/P2pP1p1/1P1P4/2r2rPP/R5K1 w - - 1 35",
+    blunderMove: "h3",
+    solution: "Rxg2+",
+  },
+  {
+    id: "3",
+    fen: "1r3r1k/4Nppp/p2pb3/q3p3/4P1P1/P4P2/1PP2Q1P/1K1R3R w - - 1 24",
+    blunderMove: "Nc6",
+    solution: "Qxa3",
+  },
+  {
+    id: "4",
+    fen: "5rk1/R1R5/2N3rp/p2p1pp1/1b6/1P2P1P1/P4PP1/6K1 b - - 5 31",
+    blunderMove: "f4",
+    solution: "Ne5",
+  },
+  {
+    id: "5",
+    fen: "3r3k/pb4pp/1pq1Bp2/5P2/P7/1P2Q1P1/4R2P/6K1 w - - 3 32",
+    blunderMove: "Rd2",
+    solution: "Qc1+",
+  },
+  {
+    id: "6",
+    fen: "3qb1k1/8/3P1ppb/1p2Q3/2pB4/2P4P/r3N1P1/1R4K1 w - - 0 36",
+    blunderMove: "Qxb5",
+    solution: "Bxb5",
+  },
+];
 
 function App() {
+  const [tactic, setTactic] = useState(tactics[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center">
+      <h1>Tactics Trainer</h1>
+      <TacticBoard
+        key={tactic.id}
+        tactic={tactic}
+        onIncorrect={() => alert("Incorrect")}
+        onSolve={() => {
+          setTactic(tactics[Math.floor(Math.random() * tactics.length)]);
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/TacticBoard.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { ChessInstance, ShortMove } from "chess.js";
+import Chessboard from "chessboardjsx";
+import Tactic from "../types/Tactic";
+
+const Chess = require("chess.js");
+
+interface Props {
+  tactic: Tactic;
+  onIncorrect: () => void;
+  onSolve: () => void;
+}
+
+const TacticBoard: React.FC<Props> = ({ tactic, onIncorrect, onSolve }) => {
+  const [fen, setFen] = useState(tactic.fen);
+  const [chess] = useState<ChessInstance>(new Chess(tactic.fen));
+  const [orientation] = useState<"black" | "white">(
+    chess.turn() === "w" ? "black" : "white"
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      chess.move(tactic.blunderMove);
+      setFen(chess.fen());
+    }, 300);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleMove = (move: ShortMove) => {
+    const fullMove = chess.move(move);
+    if (fullMove) {
+      if (fullMove.san === tactic.solution) {
+        setFen(chess.fen());
+        onSolve();
+      } else {
+        chess.undo();
+        onIncorrect();
+      }
+    } else {
+      onIncorrect();
+    }
+  };
+
+  return (
+    <Chessboard
+      position={fen}
+      width={400}
+      orientation={orientation}
+      onDrop={(move) =>
+        handleMove({
+          from: move.sourceSquare,
+          to: move.targetSquare,
+          promotion: "q",
+        })
+      }
+    />
+  );
+};
+
+export default TacticBoard;
